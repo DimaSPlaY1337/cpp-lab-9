17d2024688918d5674796d7002126f8d1860ac09
Dima Galitsyn <dimasplay1337@gmail.com>
2024-01-17 19:29:09 +0300

    add file

---
+++ cpp-lab-9.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Num
+{
+
+};
+
+template <typename T>
+float Function(T* massive, int n, string type)
+{
+    float sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (type != "float" && type != "double")
+            sum += (int)massive[i];
+        else
+            sum += massive[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    int massiv1[] = {1, 2, 3};
+    char massiv2[] = "sdfsdf";
+    Num* massiv3[] = { new Num() };
+    float massiv4[] = { 1.2, 3.4 };
+    cout << Function(massiv4, 2, "float");
+    
+    delete massiv3[0];
+}
